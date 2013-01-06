9d823f958f74991bee4ad3aa547e3c3400ed47f7
Ryan Matson <thehawox@gmail.com>
2013-01-06 16:41:00 -0700

    Forgot to add some files to the last commit

---
+++ ThreeSwords/src/name_pending/Pathing/Pathable.java
@@ -0,0 +1,46 @@
+package name_pending.Pathing;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class Pathable {
+	/*List of points that creates the path the object will take
+	 * Each point is a node in the A* algorithm
+	 */
+	protected ArrayList<Point> pathPoints = new ArrayList<Point>();
+	private AStarPathing aStarPathing;
+	
+	protected Pathable(AStarPathing asp)
+	{
+		aStarPathing = asp;
+	}
+	
+	public void needPath(Point at, Point target)
+	{
+		aStarPathing.addMe(this, at, target);
+	}
+	
+	
+	
+	//Called from the A* thread to give this a path
+	public void setPath(ArrayList<Point> newPathPoints)
+	{
+		this.pathPoints = newPathPoints;
+	}
+
+	public ArrayList<Point> getPathPoints() {
+		return pathPoints;
+	}
+
+	public void setPathPoints(ArrayList<Point> pathPoints) {
+		this.pathPoints = pathPoints;
+	}
+
+	public AStarPathing getaStarPathing() {
+		return aStarPathing;
+	}
+
+	public void setaStarPathing(AStarPathing aStarPathing) {
+		this.aStarPathing = aStarPathing;
+	}
+}
